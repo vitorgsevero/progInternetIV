3b473dea82be1a3b9d1af1dc7045c4665da6cad0
vitorgsevero <vitorgsevero@gmail.com>
2019-08-22 19:41:20 -0300

    Creating routes and adding nodemon

--- index.js
+++ index.js
@@ -2,8 +2,22 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-  res.send('Hello World!');
+  res.send('Obtendo produto!');
 });
+
+
+app.post('/', function(req, res){
+  res.send('Adicionando produto');
+});
+
+app.put('/produto', function (req, res) {
+  res.send('Atualizando produto');
+});
+
+app.delete('/produto', function (req, res) {
+  res.send('Deletando um produto');
+});
+
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
