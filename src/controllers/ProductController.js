8d58b336883ff978bf5bf45b0e63b6abcb3fd72f
vitorgsevero <vitorgsevero@gmail.com>
2019-08-29 21:23:08 -0300

    Trying CRUD Operations

--- src/controllers/ProductController.js
+++ src/controllers/ProductController.js
@@ -5,15 +5,26 @@ const Product = mongoose.model('Product');
 module.exports = {
     async index(req, res){
         
+        const products = await Product.find();
+        return res.json(products);
+
+    },
+
+    async save(req, res){
         Product.create({
             title: "React Native",
             description: "Build native apps with React",
             url: "http://github.com/facebook/react-native"
-          });
+        });
 
         const products = await Product.find();
         return res.json(products);
+    },
 
+    async deleteOne(req, res, id){
+        const product = await Product.findById(id);
+        Product.collection.findOneAndDelete(product);
+        res.json(product + "was deleted succesfully!");
     }
 
 }
--- src/routes.js
+++ src/routes.js
@@ -7,4 +7,8 @@ const ProductController = require('./controllers/ProductController');
 //First route
 routes.get("/products", ProductController.index);
 
+routes.post("/products", ProductController.save);
+
+routes.delete("/products/deleteOne/:id", ProductController.deleteOne);
+
 module.exports = routes;
