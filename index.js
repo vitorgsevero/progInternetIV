cbf1df08fcd98f8e4414d149612e4f7a4771349d
Vítor Severo <vitorgsevero@gmail.com>
2019-08-31 15:47:18 -0300

    Grant to acess the API publicly

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const requireDir = require('require-dir');
+const cors = require('cors');
 
 //Starting App
 const app = express();
 
 //To send data by JSON format
 app.use(express.json());
+
+//Grant to acess the API publicly
+app.use(cors());
 
 //Starting DB
 mongoose.connect("mongodb://localhost:27017/ppi4-node", {useNewUrlParser: true});
